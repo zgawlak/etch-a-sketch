90400eb1885b98f549b39d0cebd14d87786b6909
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-27 18:01:50 -0400

    Add mousedown logic

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const container = document.querySelector('.container');
 
+let mouseDown = false;
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+
 function createGrid() {
     for (let i = 0; i < 256; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
         container.appendChild(gridSquare);
-        gridSquare.addEventListener('mouseover', () =>
-        gridSquare.style.backgroundColor = "red");
+        gridSquare.addEventListener('mouseover', changeColour);
+    }
+
+    function changeColour(e) {
+        if (!mouseDown) return
+        else e.target.style.backgroundColor = "#000";
     }
 }
 
