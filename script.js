6969578246092bea5a84539ea7cb999c8e84ad59
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-27 18:11:36 -0400

    Add input slider

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const container = document.querySelector('.container');
-
 let mouseDown = false;
 document.body.onmousedown = () => mouseDown = true;
 document.body.onmouseup = () => mouseDown = false;
 
 function createGrid() {
+    const canvas = document.querySelector('.canvas-container');
     for (let i = 0; i < 256; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
-        container.appendChild(gridSquare);
+        canvas.appendChild(gridSquare);
         gridSquare.addEventListener('mouseover', changeColour);
     }
-
-    function changeColour(e) {
-        if (!mouseDown) return
-        else e.target.style.backgroundColor = "#000";
-    }
 }
+
+function changeColour(e) {
+    if (!mouseDown) return
+    else e.target.style.backgroundColor = "#000";
+}
+
+const slider = document.querySelector('.slider');
+const sliderOutput = document.querySelector('output');
+slider.addEventListener('input', () => sliderOutput.value = slider.value);
 
 createGrid();
