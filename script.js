f54a4eb342a28b231f3a55bca79202efd4d469a8
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-28 10:43:33 -0400

    Clean up code

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-let mouseDown = false;
-document.body.onmousedown = () => mouseDown = true;
-document.body.onmouseup = () => mouseDown = false;
-
 const canvas = document.querySelector('.canvas-container');
+const gridSize = document.querySelector('.size-input');
+const inputButton = document.querySelector('.input-button');
+let mouseDown = false;
 
 function createGrid() {
-    const gridSize = document.querySelector('.size-input');
-    const inputButton = document.querySelector('.input-button');
 
     const canvasWidth = canvas.clientWidth;
     const squareSize = canvasWidth / gridSize.value;
 
     canvas.innerHTML = '';
 
-    for (let i = 0; i < (gridSize.value * gridSize.value); i++) {
+    for (let i = 0; i < gridSize.value * gridSize.value; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
-
         gridSquare.style.width = squareSize + "px";
         gridSquare.style.height = squareSize + "px";
 
         canvas.appendChild(gridSquare);
     }
-
-    inputButton.addEventListener('click', () => {
-        createGrid();
-    });
 }
 
 function changeColour(e) {
@@ -33,6 +25,10 @@ function changeColour(e) {
     e.target.style.backgroundColor = "#000";
 }
 
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
 canvas.addEventListener('mouseover', changeColour);
+
+inputButton.addEventListener('click', createGrid);
 
 createGrid();
