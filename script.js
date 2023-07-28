d087f0cc06f1ba30ecc4fc5f60c2d60b966a4317
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-07-28 11:04:05 -0400

    Add rainbow button and mode

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const defaultMode = "black";
 const canvas = document.querySelector('.canvas-container');
 const gridSize = document.querySelector('.size-input');
 const inputButton = document.querySelector('.input-button');
+const rainbowButton = document.querySelector('.rainbow-mode');
 let mouseDown = false;
+let currentMode = defaultMode;
+
+function setCurrentMode(newMode) {
+    currentMode = newMode;
+}
+
 
 function createGrid() {
 
@@ -22,13 +30,22 @@ function createGrid() {
 
 function changeColour(e) {
     if (!mouseDown || !e.target.classList.contains('grid-square')) return;
-    e.target.style.backgroundColor = "#000";
+    if (currentMode === "rainbow") {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    }
+    else {
+        e.target.style.backgroundColor = "#000";
+    }
 }
 
 document.body.onmousedown = () => mouseDown = true;
 document.body.onmouseup = () => mouseDown = false;
 canvas.addEventListener('mouseover', changeColour);
 
+rainbowButton.addEventListener('click', () => setCurrentMode("rainbow"));
 inputButton.addEventListener('click', createGrid);
 
 createGrid();
